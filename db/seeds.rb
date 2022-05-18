fc8c460c2aecd06ca6362df46c379764a30622de
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-18 12:37:42 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Sichuan', address: 'Beijing', phone_number: '123')
+Restaurant.create(name: 'Pizza', address: 'Rome', phone_number: '456')
+Restaurant.create(name: 'Suchi', address: 'Tokyo', phone_number: '789')
+Restaurant.create(name: 'Suchi', address: 'Tokyo', phone_number: '101112')
+Restaurant.create(name: 'Croissant', address: 'Paris', phone_number: '131415')
