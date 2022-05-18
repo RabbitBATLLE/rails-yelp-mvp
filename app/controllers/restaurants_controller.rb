d9e4a26a56a1313963d1fb14bb61876f1576a315
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-18 12:28:56 +0200

    model done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,11 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurants = Restaurant.find(params[:id])
+  end
+
+
 end
